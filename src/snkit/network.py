9fe5a32fa837b672dfe368f2d8672e5d592bfd24
BenDickens <bendickens@zoho.com>
2020-07-23 14:45:03 +0200

    OD generation added by Elco, network still not fully commented or formatted

--- src/snkit/network.py
+++ src/snkit/network.py
@@ -33,15 +33,321 @@ def show(graph):
     layout = g.layout("kk")
     ig.plot(g, layout=layout)
 
-
-def graph_load(gdfNet):
-    return ig.Graph.TupleList(gdfNet.edges[['from_id','to_id','distance']].itertuples(index=False),edge_attrs=['distance'])
-
+#Creates a graph 
+def graph_load(edges):
+    #return ig.Graph.TupleList(gdfNet.edges[['from_id','to_id','distance']].itertuples(index=False),edge_attrs=['distance'])
+    g = ig.Graph(directed=False)
+    max_node_id = max(max(edges.from_id),max(edges.to_id))
+    g.add_vertices(max_node_id+1)
+    zippy = zip(edges.from_id,edges.to_id)
+    g.add_edges(zippy)
+    g.es['distance'] = edges.distance
+    g.es['time'] = edges.time
+    return g
+    
 def graph_load_largest(gdfNet):
     graph = graph_load(gdfNet)
     return graph.clusters().giant()
 
-def alt(edges, isolated_threshold=2.5, OD_no = 100, del_frac=0.02):
+#returns the largest component of a network object (network.edges pd  and network.nodes pd) with reset ids
+def largest_component_df(edges,nodes):
+    edges = edges
+    nodes = nodes
+    zippy = zip(edges['from_id'],edges['to_id'])
+    g = ig.Graph(directed=False)
+    g.add_vertices(len(nodes))
+    g.vs['id'] = nodes['id']
+    g.add_edges(zippy)
+    g.es['id'] = edges['id']
+    g = g.clusters().giant()
+    a = edges.loc[edges.id.isin(g.es()['id'])]
+    b = nodes.loc[nodes.id.isin(g.vs()['id'])]
+    return reset_ids(a,b)
+
+# This function creates a demand matrix from the equation Demand_a,b = Population_a * Population_b * e^ [-p * Distance_a,b] , see mthodology.doc
+#p is set to 1, populations represent the grid square of the origin, 
+#OD_nodes is a list of nodes to use for the OD, a,b
+#OD_orig is a shortest path matrix used for the distance calculation
+#node_pop gives the population per OD node
+def create_demand(OD_nodes, OD_orig, node_pop):
+    demand = np.zeros((len(OD_nodes), len(OD_nodes)))
+
+    dist_decay = 1
+    maxtrips = 100
+
+    for o in range(0, len(OD_nodes)):
+        for d in range(0, len(OD_nodes)):
+            if o == d:
+                demand[o][d] = 0
+            else:
+                normalized_dist = OD_orig[o,d] / OD_orig.max()
+                demand[o][d] = ((node_pop[o] * node_pop[d]) * np.exp(-1 * dist_decay * normalized_dist))
+
+    demand = ((demand / demand.max()) * maxtrips)
+    demand = np.ceil(demand).astype(int)
+
+def chooseOD(gridDF, nodes):
+    #possible_OD = np.random.choice(pop_density, size=len(pop_density), p=pop_density/pop_density.max())
+    nodeIDs = []
+    sindex = pyg.STRtree(nodes['geometry'])
+
+    for i in gridDF.itertuples():
+        print(i.geometry)
+        print(type(i.geometry))
+        ID = nearest(i.geometry, nodes, sindex)
+        print(ID)
+
+
+def nearest(geom, gdf,sindex):
+    """Find the element of a GeoDataFrame nearest a shapely geometry
+    """
+    #sindex = pygeos.STRtree(gdf['geometry'])
+    matches_idx = sindex.query(geom)
+    #pygeos.measurement.bounds(geom)
+    #matches_idx = gdf.sindex.nearest(geom.bounds)
+    nearest_geom = min(
+        [gdf.iloc[match_idx] for match_idx in matches_idx],
+        key=lambda match: pyg.measurement.distance(match.geometry,geom)
+    )
+    return nearest_geom
+
+def perc_Final(edges, del_frac, OD_list=[]):
+    result_df = []
+    OD_no = 100
+    g = graph_load(edges)
+    if OD_list == []: OD_nodes = random.sample(range(g.vcount()-1),OD_no)
+    else: OD_nodes = OD_list
+
+    edge_no = g.ecount() 
+    OD_node_no = len(OD_nodes)
+
+    node_pop = random.sample(range(4000), OD_no)
+
+
+    base_shortest_paths = g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='time')
+    OD_orig = np.matrix(base_shortest_paths)
+    OD_thresh = OD_orig * 10
+    
+    demand = create_demand(OD_nodes, OD_orig, node_pop)
+    print(demand)
+
+    exp_g = g.copy()
+    trips_possible = True
+    pos_trip_no = (((OD_node_no**2) - OD_node_no) / 2) - ((np.count_nonzero(np.isinf(OD_orig)))/2)
+    counter = 0
+    frac_counter = 0 
+
+    while trips_possible:
+        exp_edge_no = exp_g.ecount()
+
+        no_edge_del = math.floor(del_frac * edge_no)
+        try:
+            edges_del = random.sample(range(exp_edge_no),no_edge_del)
+        except:
+            print("random sample playing up but its ok")
+            edges_del = range(exp_edge_no)
+        exp_g.delete_edges(edges_del)
+        frac_counter += del_frac
+        new_shortest_paths = exp_g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
+        perc_matrix = np.matrix(new_shortest_paths)
+        results = SummariseOD(perc_matrix, 99999999999, demand, OD_orig, 50000,round(frac_counter,3))  
+        result_df.append(results)
+        
+        
+        if results[0] >= 0.99: break
+        if exp_edge_no < 1: break
+        #if counter==4: break
+    result_df = pd.DataFrame(result_df, columns=['frac_counter', 'pct_isolated', 'average_time_disruption', 'pct_thirty_plus', 'pct_twice_plus', 'pct_thrice_plus','total_surp_loss_e1', 'total_pct_surplus_loss_e1', 'total_surp_loss_e2', 'total_pct_surplus_loss_e2'])
+    print(result_df)
+
+
+def simple_OD_calc(OD, comparisonOD,pos_trip_no):
+    compare_thresh = np.greater(OD,comparisonOD)
+    over_thresh_no = np.sum(compare_thresh) / 2
+    return over_thresh_no / pos_trip_no
+    
+def SummariseOD(OD, fail_value, demand, baseline, GDP_per_capita, frac_counter):
+
+    ans = []
+
+    ### Function returns the % of total trips between origins and destinations that exceed fail value
+
+    ## calculate trips destroyed
+    masked_OD = np.ma.masked_greater(OD, value = (fail_value - 1))
+    masked_baseline = np.ma.masked_greater(baseline, value = (fail_value - 1))
+    adj_time = np.ma.masked_array(OD,masked_baseline.mask)
+    masked_demand = np.ma.masked_array(demand, masked_baseline.mask)
+
+    total_trips = masked_demand.sum()
+
+    isolated_trips = np.ma.masked_array(masked_demand,~masked_OD.mask)
+    isolated_trips_sum = isolated_trips.sum()
+
+    potentially_disrupted_trips = np.ma.masked_array(masked_demand,masked_OD.mask)
+    potentially_disrupted_trips_sum = potentially_disrupted_trips.sum()
+
+    try:
+        pct_isolated = (isolated_trips_sum / total_trips)
+    except:
+        pct_isolated  = 0
+
+    ## Set up for ratio-based calculations
+
+    fail_ratio = 50.0 #((fail_value-1) / baseline.max()) # headlimit above which trip destoryed
+
+    potentially_disrupted_trips_original_time = np.ma.masked_array(masked_baseline, masked_OD.mask)
+    delta_time_OD = (masked_OD - potentially_disrupted_trips_original_time)
+    average_time_disruption = (delta_time_OD * potentially_disrupted_trips).sum() / potentially_disrupted_trips.sum()
+
+    frac_OD = masked_OD / potentially_disrupted_trips_original_time
+
+    def PctDisrupt(x, frac_OD, demand):
+        masked_frac_OD = np.ma.masked_inside(frac_OD, 1, (1+x))
+        m_demand = np.ma.masked_array(demand, masked_frac_OD.mask)
+        return ((m_demand.sum()) / (demand.sum()))
+
+    pct_thirty_plus = PctDisrupt(0.3, frac_OD, potentially_disrupted_trips)
+    pct_twice_plus = PctDisrupt(2, frac_OD, potentially_disrupted_trips)
+    pct_thrice_plus = PctDisrupt(3, frac_OD, potentially_disrupted_trips)
+
+    # Flexing demand with trip cost
+    def surplus_loss(e, C2, C1, D1):
+
+        Y_intercept_max_cost = C1 - (e * D1)
+
+        C2 = np.minimum(C2, Y_intercept_max_cost)
+
+        delta_cost = C2 - C1
+
+        delta_demand = (delta_cost / e)
+
+        D2 = (D1 + delta_demand)
+
+        surplus_loss_ans = ((delta_cost * D2) + ((delta_cost * -delta_demand) / 2))
+
+        triangle = (D1 * (Y_intercept_max_cost - C1) ) / 2
+
+        total_surp_loss = surplus_loss_ans.sum()
+
+        total_pct_surplus_loss = total_surp_loss / triangle.sum()
+
+        return total_surp_loss, total_pct_surplus_loss
+
+    adj_cost = (adj_time * GDP_per_capita) / (365 * 8 * 3600)
+    baseline_cost = (masked_baseline * GDP_per_capita) / (365 * 8 * 3600)
+
+    total_surp_loss_e1, total_pct_surplus_loss_e1 = surplus_loss(-0.15, adj_cost, baseline_cost, masked_demand)
+    total_surp_loss_e2, total_pct_surplus_loss_e2 = surplus_loss(-0.36, adj_cost, baseline_cost, masked_demand)
+
+    return frac_counter, pct_isolated, average_time_disruption, pct_thirty_plus, pct_twice_plus, pct_thrice_plus,total_surp_loss_e1, total_pct_surplus_loss_e1, total_surp_loss_e2, total_pct_surplus_loss_e2
+
+
+#Resets the ids of the nodes and edges, editing the refereces in edge table 
+#using dict masking
+def reset_ids(edges, nodes):
+    nodes = nodes.copy()
+    edges = edges.copy()
+    to_ids =  edges['to_id'].to_numpy()
+    from_ids = edges['from_id'].to_numpy()
+    new_node_ids = range(len(nodes))
+    #creates a dictionary of the node ids and the actual indices
+    id_dict = dict(zip(nodes.id,new_node_ids))
+    nt = np.copy(to_ids)
+    nf = np.copy(from_ids) 
+    #updates all from and to ids, because many nodes are effected, this
+    #is quite optimal approach for large dataframes
+    for k,v in id_dict.items():
+        nt[to_ids==k] = v
+        nf[from_ids==k] = v
+    edges.drop(labels=['to_id','from_id'],axis=1,inplace=True)
+    edges['from_id'] = nf
+    edges['to_id'] = nt
+    nodes.drop(labels=['id'],axis=1,inplace=True)
+    nodes['id'] = new_node_ids
+    edges['id'] = range(len(edges))
+    edges.reset_index(drop=True,inplace=True)
+    nodes.reset_index(drop=True,inplace=True)
+    return edges,nodes
+
+#Del frac is now a fraction of total edge length rather than of number of edges
+def percolation_by_length(graph, OD_nodes, del_frac,isolated_threshold=2.5):
+    g = graph
+ 
+    interval_no = 2
+    edge_no = g.ecount() #10
+    OD_node_no = len(OD_nodes)
+    print("Number of Edges: ",edge_no)
+    #larg = graph.clusters().giant()
+    #print(larg.ecount())
+    intervals = np.arange(0.01,1,0.2)
+    tot_edge_length = np.sum(g.es['distance'])
+    
+    base_shortest_paths = g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
+    OD_orig = np.matrix(base_shortest_paths)
+    #print(OD_orig)
+    isolated_threshold = isolated_threshold
+    #If the graph is fully connnected this is the number of possible trips
+    theo_pos_trip_no = ((OD_node_no**2) - OD_node_no) / 2
+    #counts the trips that are not possible before percolation, this shouldn't happen if a fully connected graph is passed
+    imp_trips = (np.count_nonzero(np.isinf(OD_orig)))/2
+    #consider passing a fully connected graph or ensure OD are part of giant component
+    if imp_trips > 0: print(imp_trips," trips were not possible in the OD matrix")
+    pos_trip_no = theo_pos_trip_no - imp_trips
+    #print(pos_trip_no)
+    
+    OD_thresh = OD_orig*isolated_threshold
+    del_frac = del_frac
+    exp_g = g.copy()
+    #print(OD_thresh)
+    trips_possible = True
+    counter = 0
+    counterMax = 1/del_frac
+    thresh = del_frac*tot_edge_length
+    over_thresh_no = 0
+
+    isolated_trip_results = [0]
+    while trips_possible:
+        exp_edge_no = exp_g.ecount()
+        if exp_edge_no < 1: break
+        edges_del =  random.randint(0,exp_edge_no-1)
+        exp_g.delete_edges(edges_del)
+        if(tot_edge_length-np.sum(exp_g.es['distance'])>thresh):
+            new_shortest_paths = exp_g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
+            perc_matrix = np.matrix(new_shortest_paths)
+            result = simple_OD_calc(perc_matrix, OD_thresh, pos_trip_no)    
+            isolated_trip_results.append(result)
+            thresh += del_frac*tot_edge_length
+            counter += 1
+        if over_thresh_no >= pos_trip_no: break
+ 
+        #showMore(exp_g)
+        
+        #if counter > counterMax: break
+    
+    
+    x_ax = []
+    frac_inc = 0
+    for i in isolated_trip_results:
+        x_ax.append(frac_inc)
+        frac_inc += del_frac
+
+    return x_ax,isolated_trip_results
+    
+
+
+'''
+    for frac in intervals:
+        no_edge_del = math.floor(frac * edge_no)
+        edges_del = random.sample(range(edge_no),no_edge_del)
+        g.delete_edges(edges_del)
+        for e in g.es:
+            print(e)
+        print(edges_del)
+        showMore(g)
+        print(frac)
+        print(no_edge_del)
+'''
+def alt(edges, isolated_threshold=3, OD_no = 100, del_frac=0.02):
     g = ig.Graph(directed=False)
     max_node_id = max(max(edges.from_id),max(edges.to_id))
     g.add_vertices(max_node_id+1)
@@ -51,7 +357,7 @@ def alt(edges, isolated_threshold=2.5, OD_no = 100, del_frac=0.02):
     randomOD = random.sample(range(g.vcount()-1),OD_no)
     return percolation(g,randomOD,del_frac, isolated_threshold)
 
-def edge_to_perc(edges, isolated_threshold = 2.5, OD_no=100, del_frac=0.02):
+def edge_to_perc(edges, isolated_threshold = 3, OD_no=100, del_frac=0.02):
     g = ig.Graph(directed=False)
     max_node_id = max(max(edges.from_id),max(edges.to_id))
     g.add_vertices(max_node_id+1)
@@ -132,11 +438,9 @@ def percolation(graph, OD_nodes, del_frac, isolated_threshold=2):
         exp_g.delete_edges(edges_del)
         new_shortest_paths = exp_g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
         perc_matrix = np.matrix(new_shortest_paths)
-
-        compare_thresh = np.greater(perc_matrix,OD_thresh)
-        over_thresh_no = np.sum(compare_thresh) / 2 
-        isolated_trip_results.append(over_thresh_no/pos_trip_no)
-        if over_thresh_no >= pos_trip_no: break
+        result = simple_OD_calc(perc_matrix, OD_thresh, pos_trip_no)    
+        isolated_trip_results.append(result)
+        if result >= 1: break
  
         #showMore(exp_g)
         counter += 1
@@ -157,86 +461,6 @@ def percolation(graph, OD_nodes, del_frac, isolated_threshold=2):
     plt.show()
     print(isolated_trip_results)
 '''
-
-#Del frac is now a fraction of total edge length rather than of number of edges
-def percolation_by_length(graph, OD_nodes, del_frac,isolated_threshold=2.5):
-    g = graph
- 
-    interval_no = 2
-    edge_no = g.ecount() #10
-    OD_node_no = len(OD_nodes)
-    print("Number of Edges: ",edge_no)
-    #larg = graph.clusters().giant()
-    #print(larg.ecount())
-    intervals = np.arange(0.01,1,0.2)
-    tot_edge_length = np.sum(g.es['distance'])
-    
-    base_shortest_paths = g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
-    OD_orig = np.matrix(base_shortest_paths)
-    #print(OD_orig)
-    isolated_threshold = isolated_threshold
-    #If the graph is fully connnected this is the number of possible trips
-    theo_pos_trip_no = ((OD_node_no**2) - OD_node_no) / 2
-    #counts the trips that are not possible before percolation, this shouldn't happen if a fully connected graph is passed
-    imp_trips = (np.count_nonzero(np.isinf(OD_orig)))/2
-    #consider passing a fully connected graph or ensure OD are part of giant component
-    if imp_trips > 0: print(imp_trips," trips were not possible in the OD matrix")
-    pos_trip_no = theo_pos_trip_no - imp_trips
-    #print(pos_trip_no)
-    
-    OD_thresh = OD_orig*isolated_threshold
-    del_frac = del_frac
-    exp_g = g.copy()
-    #print(OD_thresh)
-    trips_possible = True
-    counter = 0
-    counterMax = 1/del_frac
-    thresh = del_frac*tot_edge_length
-    over_thresh_no = 0
-
-    isolated_trip_results = [0]
-    while trips_possible:
-        exp_edge_no = exp_g.ecount()
-        if exp_edge_no < 1: break
-        edges_del =  random.randint(0,exp_edge_no-1)
-        exp_g.delete_edges(edges_del)
-        if(tot_edge_length-np.sum(exp_g.es['distance'])>thresh):
-            new_shortest_paths = exp_g.shortest_paths_dijkstra(source=OD_nodes,target = OD_nodes,weights='distance')
-            perc_matrix = np.matrix(new_shortest_paths)
-            compare_thresh = np.greater(perc_matrix,OD_thresh)
-            over_thresh_no = np.sum(compare_thresh) / 2 
-            isolated_trip_results.append(over_thresh_no/pos_trip_no)
-            thresh += del_frac*tot_edge_length
-            counter += 1
-        if over_thresh_no >= pos_trip_no: break
- 
-        #showMore(exp_g)
-        
-        #if counter > counterMax: break
-    
-    
-    x_ax = []
-    frac_inc = 0
-    for i in isolated_trip_results:
-        x_ax.append(frac_inc)
-        frac_inc += del_frac
-
-    return x_ax,isolated_trip_results
-    
-
-
-'''
-    for frac in intervals:
-        no_edge_del = math.floor(frac * edge_no)
-        edges_del = random.sample(range(edge_no),no_edge_del)
-        g.delete_edges(edges_del)
-        for e in g.es:
-            print(e)
-        print(edges_del)
-        showMore(g)
-        print(frac)
-        print(no_edge_del)
-'''
     
 #https://plotly.com/python/v3/igraph-networkx-comparison/
 def showMore(graph):
@@ -244,7 +468,7 @@ def showMore(graph):
   
     layt=g.layout('kk')
     N = g.vcount()
-    labels = list(g.vs['name'])
+    labels = list(g.vs['id'])
     E = [e.tuple for e in g.es]
     Xn=[layt[k][0] for k in range(N)]
     Yn=[layt[k][1] for k in range(N)]
@@ -310,5 +534,7 @@ def showMore(graph):
     fig=Figure(data=data, layout=layout)
     py.iplot(fig, filename='network-igraph')
 
+
+
 if __name__ == '__main__':     
-    graph_example()
+    largest_component_df()
---
+++ src/snkit/population_OD.py
@@ -0,0 +1,136 @@
+import os
+import pygeos
+import geopandas as gpd
+import pandas as pd
+import numpy
+from tqdm import tqdm
+from pgpkg import Geopackage
+import warnings
+from shapely.wkb import loads,dumps
+warnings.filterwarnings("ignore")
+from multiprocessing import Pool,cpu_count
+from rasterstats import zonal_stats
+
+def create_bbox(df):
+    """Create bbox around dataframe
+    Arguments:
+
+    Returns:
+
+    """
+    return pygeos.creation.box(pygeos.total_bounds(df.geometry)[0],
+                                  pygeos.total_bounds(df.geometry)[1],
+                                  pygeos.total_bounds(df.geometry)[2],
+                                  pygeos.total_bounds(df.geometry)[3])
+
+def create_grid(bbox,height):
+    """Create a vector-based grid
+    
+    Arguments:
+
+    Returns:
+    """
+    
+    # set xmin,ymin,xmax,and ymax of the grid
+    xmin, ymin = pygeos.total_bounds(bbox)[0],pygeos.total_bounds(bbox)[1]
+    xmax, ymax = pygeos.total_bounds(bbox)[2],pygeos.total_bounds(bbox)[3]
+    
+    #estimate total rows and columns
+    rows = int(numpy.ceil((ymax-ymin) / height))
+    cols = int(numpy.ceil((xmax-xmin) / height))
+
+    # set corner points
+    x_left_origin = xmin
+    x_right_origin = xmin + height
+    y_top_origin = ymax
+    y_bottom_origin = ymax - height
+
+    # create actual grid
+    res_geoms = []
+    for countcols in range(cols):
+        y_top = y_top_origin
+        y_bottom = y_bottom_origin
+        for countrows in range(rows):
+            res_geoms.append(pygeos.polygons(
+                ((x_left_origin, y_top), (x_right_origin, y_top),
+                (x_right_origin, y_bottom), (x_left_origin, y_bottom)
+                )))
+            y_top = y_top - height
+            y_bottom = y_bottom - height
+        x_left_origin = x_left_origin + height
+        x_right_origin = x_right_origin + height
+
+    return res_geoms
+
+def create_country_OD_points(country):
+    """Create a list of OD points for the specified country 
+    
+    Arguments:
+
+    Returns:
+    """
+    
+        # set paths to data
+        world_pop = 'C:\Data\worldpop\ppp_2018_1km_Aggregated.tif'
+                
+        #get country ID
+        GID_0 = country['GID_0']
+        print('{} started!'.format(GID_0))
+        
+        #create dataframe of country row
+        df = pd.DataFrame(country).T
+        df.geometry = pygeos.from_wkb(df.geometry.values[0])
+        #specify height of cell in the grid and create grid of bbox
+        height = numpy.sqrt(pygeos.area(df.geometry)/100).values[0]
+        grid = pd.DataFrame(create_grid(create_bbox(df),height),columns=['geometry'])
+
+        #clip grid of bbox to grid of the actual spatial exterior of the country
+        clip_grid = pygeos.intersection(grid,df.geometry)
+        clip_grid = clip_grid.loc[~pygeos.is_empty(clip_grid.geometry)]
+
+        # turn to shapely geometries again for zonal stats
+        clip_grid.geometry = pygeos.to_wkb(clip_grid.geometry)
+        clip_grid.geometry = clip_grid.geometry.apply(loads)
+        clip_grid = gpd.GeoDataFrame(clip_grid)
+
+        # get total population per grid cell
+        clip_grid['tot_pop'] = clip_grid.geometry.apply(lambda x: zonal_stats(x,world_pop,stats="sum"))
+        clip_grid['tot_pop'] = clip_grid['tot_pop'].apply(lambda x: x[0]['sum'])    
+
+        # remove cells in the grid that have no population data
+        clip_grid = clip_grid.loc[~pd.isna(clip_grid.tot_pop)]
+        clip_grid = clip_grid.loc[clip_grid.tot_pop > 100]
+        clip_grid.reset_index(inplace=True,drop=True)
+        clip_grid.geometry = clip_grid.geometry.centroid
+        clip_grid['GID_0'] = GID_0
+        
+        print('{} finished!'.format(GID_0))
+        
+        clip_grid.to_csv(os.path.join('..','country_OD_points','{}.csv'.format(GID_0)))
+        # return the country 
+        return clip_grid 
+
+def create_OD_points():
+      
+    #load data and convert to pygeos
+    gdf = gpd.read_file('C:\Data\GADM\gadm36_levels.gpkg',layer=0)
+    tqdm.pandas(desc='Convert geometries to pygeos')
+    gdf = pd.DataFrame(gdf)
+    gdf['geometry'] = gdf.geometry.progress_apply(dumps)
+    
+    # create OD points
+    save_points_per_country = []
+    list_countries = list([x[1] for x in gdf.iterrows()])
+    
+    #multiprocess all countries
+    with Pool(cpu_count()-1) as pool: 
+        save_points_per_country = pool.map(create_country_OD_points,list_countries,chunksize=1) 
+           
+    df_all = pd.concat(save_points_per_country)
+    df_all['geometry'] = df_all.geometry.progress_apply(lambda x: pygeos.from_shapely(x))
+    df_all.reset_index(inplace=True,drop=True)
+    df_all.to_csv('od_points_per_country.csv')
+    
+if __name__ == "__main__":
+    # execute only if run as a script
+    create_OD_points()    
