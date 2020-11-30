f57b365843d67afb8b9a444d45eb8e01ebeb50e2
Koks <eks510@node241.cm.cluster>
2020-11-30 17:18:55 +0100

    add labels

--- src/trails/visualize_results.py
+++ src/trails/visualize_results.py
@@ -35,61 +35,64 @@ def main():
 
         # get name of percolation analysis
         net_name = file[:5]
-        try:
+        #try:
 
-            # load metrics
-            df_metrics = pd.read_csv(os.path.join(data_path_met,[x for x in met_files if file[:5] in x][0]))
-            
-            # load percolation results
-            df = pd.read_csv(os.path.join(data_path_perc,file),index_col=[0])
-            df_isolated = pd.DataFrame([df.frac_counter.values,df.pct_isolated.values]).T
-            df_isolated.columns = ['frac_counter','pct_isolated']
-
-            # load network
-            network = pd.read_feather(os.path.join(data_path_net,[x for x in net_files if file[:5] in x][0]))
-            network.geometry = pygeos.from_wkb(network.geometry)
-            network = gpd.GeoDataFrame(network)
-            network.crs = 4326
-
-            # get std and y values
-            error = df_isolated.groupby('frac_counter').std()['pct_isolated'].values
-            y = df_isolated.groupby('frac_counter').mean()['pct_isolated'].values
-                
-            mainnet = 'yes'
-            if net_name[4] != '0':
-                mainnet = 'no, #{}'.format(net_name)
-            
-            if iter1 > 0:
-                for iter2,ax in enumerate(axs.flatten()):
-                    ax.clear()
-            
-            #and plot
-            for iter2,ax in enumerate(axs.flatten()):
-
+        # load metrics
+        df_metrics = pd.read_csv(os.path.join(data_path_met,[x for x in met_files if file[:5] in x][0]))
         
-                if iter2 == 0:
-                    ax.plot(x, y, 'r-')
-                    ax.fill_between(x, y-error, y+error)
+        # load percolation results
+        df = pd.read_csv(os.path.join(data_path_perc,file),index_col=[0])
+        df_isolated = pd.DataFrame([df.frac_counter.values,df.pct_isolated.values]).T
+        df_isolated.columns = ['frac_counter','pct_isolated']
 
-                    ax.text(50, 0.2,'Main Network: {} \nEdges: {} \nDensity: {} \nClique_No: {} \nAssortativity: {} \nDiameter: {} \nMax_Degree: {}'.format( 
-                                                                                                mainnet,
-                                                                                                    df_metrics.Edge_No.values[0],
-                                                                                                    np.round(df_metrics.Density.values[0],7),
-                                                                                                    df_metrics.Clique_No.values[0],
-                                                                                                    np.round(df_metrics.Assortativity.values[0],7),
-                                                                                                    df_metrics.Diameter.values[0],
-                                                                                                    df_metrics.Max_Degree.values[0],                                                                                               
-                                                                                                    ), fontsize=15)
-                else:
-                    network.plot(column='highway',legend=True,ax=ax)
-                    cx.add_basemap(ax, crs=network.crs.to_string(),alpha=0.5)
+        # load network
+        network = pd.read_feather(os.path.join(data_path_net,[x for x in net_files if file[:5] in x][0]))
+        network.geometry = pygeos.from_wkb(network.geometry)
+        network = gpd.GeoDataFrame(network)
+        network.crs = 4326
+
+        # get std and y values
+        error = df_isolated.groupby('frac_counter').std()['pct_isolated'].values*100
+        y = df_isolated.groupby('frac_counter').mean()['pct_isolated'].values*100
             
-            plt.suptitle(dict(zip(glob_info.ISO_3digit,glob_info.Country))[net_name[:3]], fontsize=20, fontweight='bold')
-            plt.draw()
-            plt.pause(5)
-            
-        except:
-            save_failed.append(net_name)
+        mainnet = 'yes'
+        if net_name[4] != '0':
+            mainnet = 'no, #{}'.format(net_name)
+        
+        if iter1 > 0:
+            for iter2,ax in enumerate(axs.flatten()):
+                ax.clear()
+        
+        #and plot
+        for iter2,ax in enumerate(axs.flatten()):
+
+    
+            if iter2 == 0:
+                ax.plot(x, y, 'r-')
+                ax.fill_between(x, y-error, y+error)
+
+                ax.text(50, 0.2,'Main Network: {} \nEdges: {} \nDensity: {} \nClique_No: {} \nAssortativity: {} \nDiameter: {} \nMax_Degree: {}'.format( 
+                                                                                            mainnet,
+                                                                                                df_metrics.Edge_No.values[0],
+                                                                                                np.round(df_metrics.Density.values[0],7),
+                                                                                                df_metrics.Clique_No.values[0],
+                                                                                                np.round(df_metrics.Assortativity.values[0],7),
+                                                                                                df_metrics.Diameter.values[0],
+                                                                                                df_metrics.Max_Degree.values[0],                                                                                               
+                                                                                                ), fontsize=15)
+                ax.set_ylabel('Percentage of trips isolated', fontsize=15, fontweight='bold')
+                ax.set_xlabel('Percentage of network removed', fontsize=15, fontweight='bold')                                                                                                    
+            else:
+                network.plot(column='highway',legend=True,ax=ax)
+                cx.add_basemap(ax, crs=network.crs.to_string(),alpha=0.5)
+
+
+        plt.suptitle(dict(zip(glob_info.ISO_3digit,glob_info.Country))[net_name[:3]], fontsize=20, fontweight='bold')
+        plt.draw()
+        plt.pause(5)
+        
+        # except:
+        #     save_failed.append(net_name)
         
 if __name__ == '__main__':       
     main()
